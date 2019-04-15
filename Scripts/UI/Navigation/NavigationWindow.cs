84f80cda4cfaf6d193640f7b295bde5166e2c12c
jw5514 <jw5514@ic.ac.uk>
2019-04-15 18:51:34 +0100

    Fixed minimap scale

--- Scripts/UI/Navigation/NavigationWindow.cs
+++ Scripts/UI/Navigation/NavigationWindow.cs
@@ -9,24 +9,11 @@ namespace Drones.UI
     {
         public override WindowType Type { get; } = WindowType.Navigation;
         protected override Vector2 MinimizedSize { get; } = new Vector2(315, 85);
-        protected override Vector2 MaximizedSize { get; } = new Vector2(375, 480);
+        protected override Vector2 MaximizedSize { get; } = new Vector2(315, 480);
 
-        [SerializeField]
-        private GameObject _FilterPanel;
         [SerializeField]
         private GameObject _GPS;
 
-        public GameObject FilterPanel
-        {
-            get
-            {
-                if (_FilterPanel == null)
-                {
-                    _FilterPanel = ContentPanel.transform.Find("Filter Panel").gameObject;
-                }
-                return _FilterPanel;
-            }
-        }
 
         public GameObject GPS
         {
@@ -50,26 +37,9 @@ namespace Drones.UI
                 ContentPanel.transform.Find("Minimap").gameObject,
                 ContentPanel.transform.Find("Altimeter").gameObject,
                 ContentPanel.transform.Find("Compass").gameObject,
-                FilterPanel
             };
 
             base.Awake();
-        }
-
-        protected override void MinimizeWindow()
-        {
-            base.MinimizeWindow();
-            Vector2 v = GPS.transform.ToRect().offsetMax;
-            v.x = 0;
-            GPS.transform.ToRect().offsetMax = v;
-        }
-
-        protected override void MaximizeWindow()
-        {
-            base.MaximizeWindow();
-            Vector2 v = GPS.transform.ToRect().offsetMax;
-            v.x = -FilterPanel.transform.ToRect().sizeDelta.x;
-            GPS.transform.ToRect().offsetMax = v;
         }
     }
 
