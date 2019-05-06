6350527fc51b566b4da941051bc6847ff829aae4
jw5514 <jw5514@ic.ac.uk>
2019-05-07 00:55:32 +0100

    Changed heap structure

--- Assets/Scripts/External Utils/MaxHeap.cs
+++ Assets/Scripts/External Utils/MaxHeap.cs
@@ -8,37 +8,6 @@ namespace Drones.Utils
         public MaxHeap(Comparison<T> comparer) : base(comparer) { }
 
         #region Overrides
-        public override void ReSort(Comparison<T> comparer)
-        {
-            var tmp = new List<T>();
-            while (!IsEmpty())
-            {
-                tmp.Add(Remove());
-            }
-            Clear();
-            _comparer = comparer;
-            while (tmp.Count > 0)
-            {
-                Add(tmp[0]);
-                tmp.RemoveAt(0);
-            }
-        }
-
-        public override void ReSort()
-        {
-            var tmp = new List<T>();
-            while (!IsEmpty())
-            {
-                tmp.Add(Remove());
-            }
-            Clear();
-            while (tmp.Count > 0)
-            {
-                Add(tmp[0]);
-                tmp.RemoveAt(0);
-            }
-        }
-
         protected override bool Compare(T entity1, T entity2)
         {
             return _comparer(entity1, entity2) >= 0;
