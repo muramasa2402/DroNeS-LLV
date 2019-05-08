65b824b02a62def1aa2c5fcbbe470bcd7bb409bf
Andrew Chow <andrewcxw@gmail.com>
2019-05-08 21:33:22 +0100

    Added field to determine the current status of the drone

--- Assets/Scripts/Serializable Containers/RouterPayload.cs
+++ Assets/Scripts/Serializable Containers/RouterPayload.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using Drones.Utils;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,7 +12,8 @@ namespace Drones.Serializable
         public List<SVector3> dronePositions;
         public List<SVector3> droneDirections;
 
-        public Vector3 origin { get; internal set; }
-        public Vector3 destination { get; internal set; }
+        public Vector3 origin { get; set; }
+        public Vector3 destination { get; set; }
+        public JobStatus status { get; set; }
     }
 }
