ebfe82135f4459726ea0fe81bb2ba975a625bf1d
Andrew Chow <andrewcxw@gmail.com>
2019-05-09 00:43:19 +0100

    Removed getters and setters for RouterPayload

--- Assets/Scripts/Serializable Containers/RouterPayload.cs
+++ Assets/Scripts/Serializable Containers/RouterPayload.cs
@@ -9,11 +9,12 @@ namespace Drones.Serializable
     public class RouterPayload
     {
         public List<SNoFlyZone> noFlyZones;
-        public List<SVector3> dronePositions;
-        public List<SVector3> droneDirections;
+        public List<Vector3> dronePositions;
+        public List<Vector3> droneDirections;
 
-        public Vector3 origin { get; set; }
-        public Vector3 destination { get; set; }
-        public JobStatus status { get; set; }
+        public Vector3 origin;
+        public Vector3 destination;
+        public bool onJob;
+        public JobStatus status;
     }
 }
